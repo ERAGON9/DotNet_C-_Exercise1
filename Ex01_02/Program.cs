b6e557def9c8c7626961b1c8c7957b5ce98d14eb
Noa Halali <noaha@mta.ac.il>
2024-04-25 18:49:46 +0300

    .

---
+++ Ex01_02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int diamondHeight = 9;
+            PrintAsterisksDiamond(diamondHeight);
+
+            System.Console.WriteLine("\nPlease press 'Enter' to exit");
+            System.Console.ReadLine();
+        }
+
+        public static void PrintAsterisksDiamond(int i_DiamondHeight)
+        {
+            int firstRow = 0;
+
+            PrintAsterisksDiamondHelper(i_DiamondHeight, firstRow);
+        }
+        public static void PrintAsterisksDiamondHelper(int i_DiamondHeight, int i_CurrentRow)
+        {
+            int rowWidth = i_DiamondHeight;
+            int middleRow = i_DiamondHeight / 2;
+            int numberOfAsterisksInCurrentRow = 2 * i_CurrentRow + 1;
+            int numberOfSpacesInRow = rowWidth - numberOfAsterisksInCurrentRow;
+            string spacesString = new string(' ', numberOfSpacesInRow/2);
+            StringBuilder resultString = new StringBuilder(spacesString);
+            string asterisksString = new string('*', numberOfAsterisksInCurrentRow);
+            bool isDoneRecursion = (middleRow == i_CurrentRow);
+
+            resultString.Append(asterisksString);
+            resultString.Append(spacesString);
+            System.Console.WriteLine(resultString);
+            if (isDoneRecursion)
+            {
+                return;
+            }
+            else
+            {
+                PrintAsterisksDiamondHelper(i_DiamondHeight, i_CurrentRow +1);
+                System.Console.WriteLine(resultString);
+            }
+        }
+    }
+}
---
+++ Ex01_03/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex01_02;
+
+namespace Ex01_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int height = GetHeightFromUser();
+
+            Ex01_02.Program.PrintAsterisksDiamond(height);
+
+            System.Console.WriteLine("\nPlease press 'Enter' to exit");
+            System.Console.ReadLine();
+        }
+
+        public static int GetHeightFromUser()
+        {
+            int height = 0;
+            Console.WriteLine("Please enter diamond height:");
+            string heightStr = Console.ReadLine();
+            System.Console.WriteLine();
+            bool isValidInput = IsValidInput(heightStr, ref height);
+
+            while(!isValidInput)
+            {
+                Console.WriteLine("Please enter diamond height:");
+                heightStr = Console.ReadLine();
+                isValidInput = IsValidInput(heightStr, ref height);
+            }
+
+            bool isEven = height % 2 == 0;
+            if(isEven)
+            {
+                height++;
+            }
+
+            return height;
+        }
+
+        public static bool IsValidInput(string i_input, ref int io_Height)
+        {
+            bool result;
+            bool isInteger = IsInteger(ref i_input, ref io_Height);
+
+            if (!isInteger)
+            {
+                Console.WriteLine("Wrong input type: height must be integer. Try again");
+                result = false;
+            }
+            else if (io_Height <= 0)
+            {
+                Console.WriteLine("Wrong input value: height must be positive. Try again");
+                result = false;
+            }
+            else
+            {
+                result = true;
+            }
+
+            return result;
+        }
+        public static bool IsInteger(ref string io_input, ref int io_Height)
+        {
+            bool success = int.TryParse(io_input, out io_Height);
+
+            return success;
+        }
+    }
+}
